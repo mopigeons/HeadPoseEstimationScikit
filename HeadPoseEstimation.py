b6f31eb99237da8a26c0716102510a5be4d363c5
simon rodier <simon@mopigeons.com>
2015-05-13 11:10:53 -0400

    formatting changes

--- HeadPoseEstimation.py
+++ HeadPoseEstimation.py
@@ -3,30 +3,31 @@ __author__ = 'Simon'
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
 
-#establish training data points (X) and their labels (Y)
-#--------------------------------------------------------
+# establish training data points (X) and their labels (Y)
+# --------------------------------------------------------
 # The current data is dummy data and needs to be replaced
-#--------------------------------------------------------
-X = [[0,1,0,0,0],[1,1,0,0,1], [0,0,1,1,1], [1,0,1,1,0], [0,1,0,0,1], [1,0,1,0,0], [1,1,1,0,1], [0,1,1,0,1]]
-Y = [0,1,1,0,1,0,1,1]
+# --------------------------------------------------------"
+X = [[0, 1, 0, 0, 0], [1, 1, 0, 0, 1], [0, 0, 1, 1, 1], [1, 0, 1, 1, 0], [0, 1, 0, 0, 1], [1, 0, 1, 0, 0],
+     [1, 1, 1, 0, 1], [0, 1, 1, 0, 1]]
+Y = [0, 1, 1, 0, 1, 0, 1, 1]
 
-#build the classification forest
+# build the classification forest
 clf = RandomForestClassifier(n_estimators=1000)
 clf = clf.fit(X, Y)
 
-#build the regression forest
+# build the regression forest
 reg = RandomForestRegressor(n_estimators=1000)
-reg = reg.fit(X,Y)
+reg = reg.fit(X, Y)
 
-#get classification and regression predictions for some new test data point
-#---------------------------------------------------------
+# get classification and regression predictions for some new test data point
+# ---------------------------------------------------------
 # Need to import new data point and translate it from
 # image to matrix or array
-#---------------------------------------------------------
-prediction = clf.predict_proba([1,1,1,1,1])
-predict2 = reg.predict([1,1,1,1,1])
+# ---------------------------------------------------------
+prediction = clf.predict_proba([1, 1, 1, 1, 1])
+predict2 = reg.predict([1, 1, 1, 1, 1])
 
-#print the results on the screen
+# print the results on the screen
 print("\nclassification")
 print(prediction)
 print("\nregression")
