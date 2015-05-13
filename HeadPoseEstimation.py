981696a0201df44d78655d54f0f293d68f20a61a
simon rodier <simon@mopigeons.com>
2015-05-13 10:29:07 -0400

    Initial commit

---
+++ HeadPoseEstimation.py
@@ -0,0 +1,26 @@
+__author__ = 'Simon'
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+
+#establish training data points (X) and their labels (Y)
+X = [[0,1,0,0,0],[1,1,0,0,1], [0,0,1,1,1], [1,0,1,1,0], [0,1,0,0,1], [1,0,1,0,0], [1,1,1,0,1], [0,1,1,0,1]]
+Y = [0,1,1,0,1,0,1,1]
+
+#build the classification forest
+clf = RandomForestClassifier(n_estimators=1000)
+clf = clf.fit(X, Y)
+
+#build the regression forest
+reg = RandomForestRegressor(n_estimators=1000)
+reg = reg.fit(X,Y)
+
+#get classification and regression predictions for some new test data point
+prediction = clf.predict_proba([1,1,1,1,1])
+predict2 = reg.predict([1,1,1,1,1])
+
+#print the results on the screen
+print("\nclassification")
+print(prediction)
+print("\nregression")
+print(predict2)
